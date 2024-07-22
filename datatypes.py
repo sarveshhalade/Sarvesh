2147046fadb6eda7bdbf5a9c5368736c9b241823
sarveshhalade <sdhalade9799@gmail.com>
2024-07-22 02:29:29 -0700

    first

---
+++ datatypes.py
@@ -0,0 +1,30 @@
+a=5
+print("Type of a is :",type(a))
+a="sarvesh"
+print("Type of a is :",type(a))
+a=60.5
+print("Type of a is :",type(a))
+a=1j
+print("Type of a is :",type(a))
+a=["geeks","for","geeks"]
+print("Type of a is :",type(a))
+a=("apple","bird","sarvesh")
+print("Type of a is :",type(a))
+a=range(6)
+print("Type of a is :",type(a))
+a={"name":"sarvesh","age":15}
+print("Type of a is :",type(a))
+a={"apple","bannana","cherry"}
+print("Type of a is :",type(a))
+a=frozenset({"apple","bannana","cherry"})
+print("Type of a is :",type(a))
+a=True
+print("Type of a is :",type(a))
+a=b"hello"
+print("Type of a is :",type(a))
+a=bytearray(4)
+print("Type of a is :",type(a))
+a=memoryview(bytearray(6))
+print("Type of a is :",type(a))
+a=None
+print("Type of a is :",type(a))
