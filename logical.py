aef96c006394fd63e9b124152faba33cbcf783da
sarveshhalade <155130162+sarveshhalade@users.noreply.github.com>
2024-07-22 20:54:06 +0530

    logical

---
+++ logical.py
@@ -0,0 +1,10 @@
+result = True and False
+print(result)  # Output: False
+
+# Logical OR
+result = True or False
+print(result)  # Output: True
+
+# Logical NOT
+result = not True
+print(result)  # Output: False
