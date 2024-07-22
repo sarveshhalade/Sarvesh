f3aca6ae92c6b0b6e09b98b7615eba888303b54c
sarveshhalade <sdhalade9799@gmail.com>
2024-07-22 02:58:07 -0700

    op

---
+++ operators1.py
@@ -0,0 +1,13 @@
+print("Assignment Opeartors")
+x=5
+print("Value of x is",(x))
+x+=5
+print("Value of x is",(x))
+x-=5
+print("Value of x is",(x))
+x*=5
+print("Value of x is",(x))
+x/=2
+print("Value of x is",(x))
+x%=4
+print("Value of x is",(x))
