5e645ee4ee8e1c152ff48b8fdf19c1c5edf00621
sarveshhalade <sdhalade9799@gmail.com>
2024-07-22 21:25:20 +0530

    rel

---
+++ relational.py
@@ -0,0 +1,19 @@
+# Equal to (==)
+a = 5
+b = 10
+print(a == b)  # Output: False
+
+# Not equal to (!=)
+print(a != b)  # Output: True
+
+# Less than (<)
+print(a < b)   # Output: True
+
+# Greater than (>)
+print(a > b)   # Output: False
+
+# Less than or equal to (<=)
+print(a <= b)  # Output: True
+
+# Greater than or equal to (>=)
+print(a >= b)  # Output: False
