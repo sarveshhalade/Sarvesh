34f388658eb6195fb89fc7225010fba33f29a440
sarveshhalade <sdhalade9799@gmail.com>
2024-08-26 14:57:16 +0530

    dict

---
+++ dict.py
@@ -0,0 +1,28 @@
+student1={"Name": "Sarvesh","Age": 21}
+print (student1)
+print (student1["Name"])
+student1["Age"]=34
+print (student1)
+student1["college"]="gkg"
+print (student1)
+del student1["college"]
+print (student1)
+print (len(student1))
+print (type(student1))
+print(student1.keys())
+print (student1.values())
+student2={"college":"dypcet","email":"a@gmail.com"}
+print (student2)
+student1.update(student2)
+print(student1)
+student1.clear()
+print(student1)
+student1={"name":"aaa"}
+print(student1)
+del student1
+print (student1)
+
+
+
+
+
